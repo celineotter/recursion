33f03949a09b2bf495301d59aba31e721bf3c1e5
celineotter <celine.otter@gmail.com>
2014-05-01 20:56:20 -0700

    switching to clearer variable names in parseJSON.js ... trying to simplify

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,17 +2,17 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
-var parseJSON = function (obj) {
-    var firstObj, subeqObj,strLength, i=0, synthaxError = false;
+var parseJSON = function (obj) {debugger;
+    var createdObj, strLength, i = 0, synthaxError = false;
 
     //account for backslashes infront of quotes within strings
-    function reduceBackstroke (gpStart,gpEnd) {
+    function checkBackstrokes (gpStart, gpEnd) {
         var str ='';
         while (gpStart < gpEnd) {
             var slashIndex = obj.indexOf('\\', gpStart);
             if(slashIndex !== -1){
-				if (slashIndex >= gpEnd-1) synthaxError = true;
-				str += obj.slice(gpStart, slashIndex) + obj.slice(slashIndex +1,slashIndex +2);
+                if (slashIndex >= gpEnd-1) synthaxError = true;
+                str += obj.slice(gpStart, slashIndex) + obj.slice(slashIndex +1, slashIndex +2);
                 gpStart = slashIndex +2;
             } else {
                 str += obj.slice(gpStart,gpEnd);
@@ -28,49 +28,46 @@ var parseJSON = function (obj) {
         var getSlashQuote = obj.indexOf('\\"', gpStart);
         
         return (getSlashQuote < getQuote && getSlashQuote !== -1 ?
-        	reduceBackstroke(gpStart,gpEnd) : obj.slice(gpStart, getQuote));
+            checkBackstrokes(gpStart,gpEnd) : obj.slice(gpStart, getQuote));
     }
 
     //numb func:
     function findNumb (gpStart) {
-        var gpEnd;
-        for (var n=gpStart; /[0-9-.]/.test(obj[n]); n++) {
-            gpEnd = n;
+        var endIndex;
+        for (var n = gpStart; /[0-9-.]/.test(obj[n]); n++) {
+            endIndex = n;
         }
-        return obj.slice(gpStart, gpEnd +1);
+        return obj.slice(gpStart, endIndex +1);
     }
     
-    function indexOf2nd (gpStart, elementOpen, elementClose) {
-        var newPairTrack = 0;
-        while(newPairTrack >= 0) {
-            var gpEnd = obj.indexOf(elementClose, gpStart);
-            var check = obj.indexOf(elementOpen, gpStart);
-            if (check < gpEnd && check !== -1) {newPairTrack++; gpStart = check +1;}
-            else {newPairTrack--; gpStart = gpEnd +1;}
+    function indexOf2nd (startIndex, openB, closeB) {
+        var newBracketCount = 0;
+        while(newBracketCount >= 0) {
+            var endIndex = obj.indexOf(closeB, startIndex);
+            var checkForNested = obj.indexOf(openB, startIndex);
+            if (checkForNested < endIndex && checkForNested !== -1) {newBracketCount++; startIndex = checkForNested +1;}
+            else {newBracketCount--; startIndex = endIndex +1;}
         }
-        return gpEnd;
+        return endIndex;
     }
     // // obj + array get inside string func:
-    function getLengthBtwnBrackets(gpStart, openBracketType, closeBracketType) {
-        var gpEnd = indexOf2nd(gpStart, openBracketType, closeBracketType);
-        if (gpEnd === -1) synthaxError = true;
-        return gpEnd-gpStart;
+    function getLengthBtwnBrackets(startIndex, openB, closeB) {
+        var endIndex = indexOf2nd(startIndex, openB, closeB);
+        if (endIndex === -1) synthaxError = true;
+        return endIndex - startIndex;
     }
 
 // same in arr and obj:
-function commonFunc(start, openQ, closeQ) {
-    if (openQ === '[') var subseqObj = [];
-    else var subseqObj = {};
-    var count = 0, propertiesArr = [], valuesArr = [];
-    var initCount = start;
+function commonFunc(start, openB, closeB) {
+    var buildingObj = (openB === '[' ? [] : {});
+    var insertKeyNext = true, keysArr = [], valuesArr = [];
+    var frozenStart = start;
     
-    var strLengthInsideBrackets = getLengthBtwnBrackets(start, openQ, closeQ);
-
-    if (strLengthInsideBrackets > 0) {
-        var objMaxLength = strLengthInsideBrackets + initCount;
+    var strLengthInsideBrackets = getLengthBtwnBrackets(start, openB, closeB);
+    var objCloseIndex = strLengthInsideBrackets + frozenStart;
         
-        while (start < objMaxLength && start !== 0) {
-            var elementFromStrObj = guideMappingByType(start, objMaxLength -1);
+        while (start < objCloseIndex && start !== 0 && strLengthInsideBrackets > 0) {
+            var elementFromStrObj = mapByType(start, objCloseIndex -1);
 
             if (typeof elementFromStrObj === 'object' && elementFromStrObj !== null && !Array.isArray(elementFromStrObj)){
                 strLength = elementFromStrObj.objToStrLength;
@@ -78,55 +75,50 @@ function commonFunc(start, openQ, closeQ) {
             }
             if (Array.isArray(elementFromStrObj)) strLength = elementFromStrObj.pop();
             
-            if (openQ === "[") subseqObj.push(elementFromStrObj);
-            else if (count === 0) {propertiesArr.push(elementFromStrObj); count++;}
-            else {valuesArr.push(elementFromStrObj); count--;}
-
-            if (typeof elementFromStrObj === 'string') start += elementFromStrObj.length +1;
-            else start += strLength;
-
-            if (count === 1) start = obj.indexOf(':', start) +1;
-            else start = obj.indexOf(',', start) +1;
+            if (openB === "[") buildingObj.push(elementFromStrObj);
+            else if (insertKeyNext) {keysArr.push(elementFromStrObj); insertKeyNext = false;}
+            else {valuesArr.push(elementFromStrObj); insertKeyNext = true;}
+            
+            start += (typeof elementFromStrObj === 'string' ? elementFromStrObj.length +1 : strLength);
+            start = obj.indexOf((insertKeyNext ? ',' : ':'), start) +1;
         }
-        if (openQ ==='{'){
+        if (openB ==='{') {
             for ( var i = 0; i < valuesArr.length; i++) {
-                subseqObj[propertiesArr[i]] = valuesArr[i];
+                buildingObj[keysArr[i]] = valuesArr[i];
             }
         }
+        if (openB === '[') buildingObj.push(strLengthInsideBrackets);
+        else {buildingObj['objToStrLength'] = strLengthInsideBrackets;}
+        return buildingObj;
     }
-        if (openQ === '[') subseqObj.push(strLengthInsideBrackets);
-        else {subseqObj['objToStrLength'] = strLengthInsideBrackets;}
-        return subseqObj;
-    }
-
-
     //===============================================//
 
-    function guideMappingByType (start, objMaxLength) {
+    function mapByType (start, objMaxLength) {
         //find string
         if (obj[start] === '"') return findString(start +1, objMaxLength);
         //find number        
-        else if (/-|[0-9]/.test(obj[start]) ) {
-            var rslt = findNumb(start); strLength = rslt.length; 
-            return Number(rslt);
+        else if (/-|[0-9]/.test(obj[start])) {
+            var numberStr = findNumb(start); strLength = numberStr.length;
+            return Number(numberStr);
         }
         //find null, true, false
         else if (obj.slice(start, start +4) === 'null') {strLength = 4; return null;}
-        else if (obj.slice(start,start +4) === 'true') {strLength = 4; return true;}
-        else if (obj.slice(start,start +5) === 'false') {strLength = 5; return false;}
+        else if (obj.slice(start, start +4) === 'true') {strLength = 4; return true;}
+        else if (obj.slice(start, start +5) === 'false') {strLength = 5; return false;}
         
         //if arr
         else if (obj[start] === '[') return commonFunc(++start, '[',']');
         // if obj
         else if (obj[start] === '{') return commonFunc(++start, '{','}');
-        else return (start < objMaxLength ? guideMappingByType(++start, objMaxLength) : undefined);
+        else return (start < objMaxLength ? mapByType(++start, objMaxLength) : undefined);
     }
     //===============================================//
-    firstObj = guideMappingByType(0);
-    if (synthaxError) firstObj = undefined;
-    if (Array.isArray(firstObj)) firstObj.pop();
-    else if(typeof firstObj === 'object' && firstObj !== null) delete firstObj['objToStrLength'];
 
-    return firstObj;
+    createdObj = mapByType(0);
+    if (synthaxError) createdObj = undefined;
+    if (Array.isArray(createdObj)) createdObj.pop();
+    else if(typeof createdObj === 'object' && createdObj !== null) delete createdObj['objToStrLength'];
+
+    return createdObj;
 
 };
