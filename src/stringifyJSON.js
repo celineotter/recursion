f636ae67446e40f98ece2888a94c3009978a41e8
celineotter <celine.otter@gmail.com>
2014-05-01 17:20:37 -0700

    simplified stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,51 +3,30 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function (obj) {
+    var historyArr = [], count = 0, resultStr;
 
-    var historyArr = [], count=0, resultStr;
+    function guideFunc (value) {
+        if (typeof value === 'string') return '"' + value + '"';
+        else if (typeof value === 'boolean' || typeof value === 'number'|| value === null) return value;
+        else if (Array.isArray(value)) return arrObjGuide(value, '[', ']');
+        else if (typeof value === 'object' && typeof obj !== 'function') return arrObjGuide(value, '{', '}');
+		else return undefined;
+    }
 
-    function guideFunc (value){
-        if (typeof value === 'string') {return '"' + value + '"';}
-        else if (typeof value === 'boolean' || typeof value === 'number'|| value === null) {return value;}
-        else if (Array.isArray(value)){return arrFunc(value);}
-        else if (typeof value === 'object' && !Array.isArray(value) && typeof obj !== 'function') {return objectFunc(value);}
-		else {return undefined;}
-    }
-    
-    //============================================================//
-    function arrFunc (obj){
-		var x = count++;
-		historyArr[x] = [];
-		for (var i=0; i<obj.length; i++) {
-			if (guideFunc(obj[i]) === undefined) {
-				continue;
-			} else {
-			historyArr[x].push(guideFunc(obj[i]));
-			}
-		}
-		return '['+ historyArr[x].join(',')+']';
-    }
-    
-    //============================================================//
-    
-    function objectFunc (obj){
+    function arrObjGuide (obj, openQ, closeQ) {
         var x = count++;
         historyArr[x] = [];
-        var objKeys = Object.keys(obj);
-    
-        for(var j=0; j<objKeys.length; j++) {
-            var key = guideFunc(objKeys[j]);
-            var value = guideFunc(obj[objKeys[j]]);
-			if (key === undefined || value === undefined) {
-				continue;
-			} else {
-				historyArr[x].push( key + ':' + value);
-			}
-        }
-    return '{' + historyArr[x].join(',') + '}';
+
+        _.each(obj, function (value, key) {
+            if (openQ === '[') historyArr[x].push(guideFunc(value));
+            else {
+                var keyStr = guideFunc(key);
+                var valueStr = guideFunc(value);
+                if (keyStr !== undefined && valueStr !== undefined) historyArr[x].push( keyStr + ':' + valueStr);
+            }
+        });
+        return openQ + historyArr[x].join(',') + closeQ;
     }
-    //============================================================//
-    if (Array.isArray(obj)) {return arrFunc(obj);}
-    else if (typeof obj === 'object' && !Array.isArray(obj) && obj !==null) {return objectFunc(obj);}
-    else {return '' + guideFunc(obj)+ '';}
+ 
+    return (typeof obj === 'object' && obj !== null ? guideFunc(obj) : '' + guideFunc(obj)+ '');
 };
